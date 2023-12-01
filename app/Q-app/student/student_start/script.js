94a4dfe556d7b76a8b075ba1f93b0cda8fd96c9b
Ken Takeo <ken20051205@gmail.com>
2023-12-02 04:10:03 +0900

    修正コミット

--- app/Q-app/student/student_start/script.js
+++ app/Q-app/student/student_start/script.js
@@ -129,7 +129,7 @@ firebase.auth().onAuthStateChanged(function (user) {
     }
   } else {
     // 未ログイン時
-    window.location.href = "../login";
+    window.location.href = "../../login";
   }
 });
 
