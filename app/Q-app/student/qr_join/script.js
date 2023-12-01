e32af3e93a427a2afcd4d01b36e4d628145a4670
Ken Takeo <ken20051205@gmail.com>
2023-12-02 04:45:37 +0900

    detaとdataミスりすぎあほが

--- app/Q-app/student/qr_join/script.js
+++ app/Q-app/student/qr_join/script.js
@@ -4,6 +4,8 @@ window.onload = function(){
     let url = new URL(url_string);
     // URLオブジェクトのsearchParamsのget関数でIDがdの値を取得する。
     let data=url.searchParams.get("class_Code");
+    console.log(data)
+    var class_Code = data;
     // "d"というIDが定義されていない場合はnullが入る
     if(class_Code != null){
         document.cookie = "class_Code = " + data + "; path=/;"
