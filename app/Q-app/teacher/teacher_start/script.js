0beac462c61b4f5c4d71cada1000d818903f6f9f
Ken Takeo <ken20051205@gmail.com>
2023-12-15 22:56:03 +0900

    パスミス

--- app/Q-app/teacher/teacher_start/script.js
+++ app/Q-app/teacher/teacher_start/script.js
@@ -192,7 +192,7 @@ firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     // ログイン時
     if (!detectTeacher(user.email,user.displayName)) {
-      window.location.href = "../student/student_start";
+      window.location.href = "../../student/student_start";
     } else {
       // Update the user information display
       var userInfoElement = document.querySelector(".user-info");
