d3b4822bb4e8c858e3659a022cebe039760f0eed
しゃけのきりみ。 <97162677+Shakenokirimi12@users.noreply.github.com>
2024-01-18 12:50:26 +0900

    これでなおるはず！

--- app/Q-app/teacher/teacher_menu/script.js
+++ app/Q-app/teacher/teacher_menu/script.js
@@ -32,7 +32,7 @@ async function sendToGAS() {
       .then((response) => response.json())
       .then((data) => {
         if (data.length != 0) {
-          window.open(data.url, "_blank");
+          window.open(data.shareurl, "_blank");
         }
         else {
           console.log(responseresult.result);
