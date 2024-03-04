6fe98d5e21669a384c6e77a447569d0f8c51d1ee
Ken Takeo <ken20051205@gmail.com>
2024-03-04 19:22:05 +0900

    googleログインの実装

---
+++ logout/script.js
@@ -0,0 +1,38 @@
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // ログイン時
+    firebase
+      .auth()
+      .signOut()
+      .then(function () {
+        Swal.fire({
+          html: "<strong>ログアウトしました。</strong>",
+          title: "情報",
+          icon: "info",
+          toast: true,
+          position: "top-end", //画面右上
+          showConfirmButton: false,
+          timer: 3000, //3秒経過後に閉じる
+        }).then((result) => {
+          document.cookie = "class_Code=; path=/;";
+          location.reload();
+        });
+      });
+  } else {
+    // 未ログイン時
+    window.location.href = "/login/index.html";
+  }
+});
+
+window.onload = function () {
+  if (
+    (navigator.userAgent.indexOf("iPhone") > 0 &&
+      navigator.userAgent.indexOf("iPad") == -1) ||
+    navigator.userAgent.indexOf("iPod") > 0 ||
+    navigator.userAgent.indexOf("Android") > 0
+  ) {
+    location.href = "../mobile";
+  }
+};
+
+
