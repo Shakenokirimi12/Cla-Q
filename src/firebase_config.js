e192e6bf2774145d5d2edceded00ecebcaf0d5b2
しゃけのきりみ。 <97162677+Shakenokirimi12@users.noreply.github.com>
2024-04-20 16:00:28 +0900

    change firebase commit.

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,10 +1,10 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyBnl2GIjQ-CMZ44tXmSxgYVcz_iRB10SYM",
-  authDomain: "cla-q-7e2d9.firebaseapp.com",
-  projectId: "cla-q-7e2d9",
-  storageBucket: "cla-q-7e2d9.appspot.com",
-  messagingSenderId: "809026902811",
-  appId: "1:809026902811:web:eb91e5c91d1020e15311be",
+  apiKey: "AIzaSyAS453tv7Rv-9SRtA38UOQVanAZeOKnR8A",
+  authDomain: "cla-q-v2.firebaseapp.com",
+  projectId: "cla-q-v2",
+  storageBucket: "cla-q-v2.appspot.com",
+  messagingSenderId: "24057564267",
+  appId: "1:24057564267:web:fbcc79c88a61e150e4352e"
 };
 firebase.initializeApp(firebaseConfig);
