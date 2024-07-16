1af0f301dad83df46c623b18d94f038500e742da
しゃけのきりみ。 <97162677+Shakenokirimi12@users.noreply.github.com>
2024-07-16 18:19:29 +0900

    Update script.js

--- login/script.js
+++ login/script.js
@@ -12,16 +12,12 @@ function onStartClick() {
           html: "Googleログインに失敗しました。<br>画面右上のポップアップ設定を許可してください。<br>内部エラー:" + error,
           title: "情報",
           icon: "error",
-        }).then((result) => {
-          window.location.href = "./";
         });
       } else {
         Swal.fire({
           html: "Googleログインに失敗しました。<br>内部エラー:" + error,
           title: "情報",
           icon: "error",
-        }).then((result) => {
-          window.location.href = "./";
         });
       }
     });
@@ -54,16 +50,12 @@ firebase.auth()
               html: "Googleログインに失敗しました。<br>画面右上のポップアップ設定を許可してください。<br>内部エラー:" + error,
               title: "情報",
               icon: "error",
-            }).then((result) => {
-              window.location.href = "./";
             });
           } else {
             Swal.fire({
               html: "Googleログインに失敗しました。<br>内部エラー:" + error,
               title: "情報",
               icon: "error",
-            }).then((result) => {
-              window.location.href = "./";
             });
           }
         });
